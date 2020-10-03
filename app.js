f458b7373a68f4bf7ec36b23f69a380609d3b4e4
odeane_croney <odeane_croney@yahoo.com>
2020-10-03 23:20:59 +0200

    adding unit to req

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ require('dotenv').config()
 const API_KEY = process.env.DB_KEY
 
 
-const url = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=37.8267,-122.4233`
+const url = `http://api.weatherstack.com/current?access_key=${API_KEY}&query=37.8267,-122.4233&units=f`
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.current)
+request({ url: url, json:true }, (error, response) => {
+  console.log(response.body.current)
+  console.log(`It is currrently ${response.body.current.temperature} but it feels like ${response.body.current.feelslike}`)
 })
